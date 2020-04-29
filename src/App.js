5c61fd66f4cbb4a0e2c5b1b8982b8f612def679e
Baruch Phillips <baruchphillips@gmail.com>
2020-04-28 21:24:02 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ItemList from './components/ItemList'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Shopping App
+    <ItemList/>
+    </div>
+  );
+}
+
+export default App;
