df5c55eb4ec55ca69c1bbd7af313f80007b54506
Baruch Phillips <baruchphillips@gmail.com>
2020-07-12 16:35:51 -0400

    filter design

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,20 +1,90 @@
-import React from 'react'
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function Filter({setFilter}) {
-    return (
-        <div  style={{
-            position: "absolute",
-            left: '10%',
-            top: '5%'}} className="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Filter Items
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-        <button value={""} onClick={() => setFilter("")} class="dropdown-item" type="button">All</button>
-          <button value={"dairy"} onClick={() => setFilter("dairy")}
-           class="dropdown-item" type="button">Dairy</button>
-          <button value={"cereal"} onClick={() => setFilter("cereal")} class="dropdown-item" type="button">Cereals</button>
-        </div>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+export default function MenuListComposition({setFilter}) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <div className={classes.root} 
+    //  style={{  position: "absolute",
+    //                 left: '10%',
+    //                 top: '5%'}}
+                    >
+      <div>
+        <Button
+          ref={anchorRef}
+          aria-controls={open ? 'menu-list-grow' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        >
+          Filter
+        </Button>
+        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                    <MenuItem value={""} onClick={() => setFilter("")} >All</MenuItem>
+                    <MenuItem value={"dairy"} onClick={() => setFilter("dairy")}>Dairy</MenuItem>
+                    <MenuItem value={"cereal"} onClick={() => setFilter("cereal")}>Cereal</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
       </div>
-    )
+    </div>
+  );
 }
